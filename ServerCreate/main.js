4d204291fac27c28f79c04b762394955968ec7bb
taimul <taimul.haque@gmail.com>
2021-03-03 15:23:44 +0600

    Server req and response

--- ServerCreate/main.js
+++ ServerCreate/main.js
@@ -1,7 +1,21 @@
 var http=require('http');
 
 var server=http.createServer(function (req,res) {
-    res.end("Hello world");
+    if(req.url=="/"){
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write('<h1>This is Home Page</h1>')
+        res.end();
+    }
+    else if(req.url=="/about"){
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write('<h1>This is About Page</h1>')
+        res.end();
+    }
+    else if(req.url=="/product"){
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write('<h1>This is Product Page</h1>')
+        res.end();
+    }
 });
 
 server.listen(5050);

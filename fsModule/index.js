0ffadffa51a42670edc59b053758cb5f5617220b
taimul <taimul.haque@gmail.com>
2021-03-09 19:52:35 +0600

    Synchonous Module

--- fsModule/index.js
+++ fsModule/index.js
@@ -1,14 +1,14 @@
 var fs=require('fs');
 var http=require('http');
 
-var server = http.createServer(function (req,res) {
+var server=http.createServer(function (req,res) {
 
     if(req.url="/"){
-        fs.readFile('Home.html',function (error,data) {
+            let mydata = fs.reaFileSync('Home.html');          //Synchronous, (no callback)
             res.writeHead(200,{'Content-type':'text/html'});
-            res.write(data);
+            res.write(mydata);
             res.end();
-        });
+
     }
 });
 server.listen(4040);

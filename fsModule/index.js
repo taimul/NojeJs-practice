bd46671b7ea90300f18f7bcb20f13d9761fb0e61
taimul <taimul.haque@gmail.com>
2021-03-09 20:32:08 +0600

    Resolved merge conflict by incorporating both suggestions.

--- fsModule/index.js
+++ fsModule/index.js
@@ -5,7 +5,11 @@ var server=http.createServer(function (req,res) {
 
     if(req.url="/"){
 <<<<<<< HEAD
+<<<<<<< HEAD
             let mydata = fs.reaFileSync('Home.html');          //Synchronous, (no callback)
+=======
+        fs.readFile('Home.html',function (error,data) {         //filepath,FileOption and callback Function
+>>>>>>> 49b7993c8e2dbf4fd0bcf0245a2250a9b4a5895e
 =======
         fs.readFile('Home.html',function (error,data) {         //filepath,FileOption and callback Function
 >>>>>>> 49b7993c8e2dbf4fd0bcf0245a2250a9b4a5895e

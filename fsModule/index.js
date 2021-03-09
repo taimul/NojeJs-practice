49b7993c8e2dbf4fd0bcf0245a2250a9b4a5895e
taimul <79805730+taimul@users.noreply.github.com>
2021-03-09 19:50:44 +0600

    Comments added for Asynchronous Module

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,7 +4,7 @@ var http=require('http');
 var server = http.createServer(function (req,res) {
 
     if(req.url="/"){
-        fs.readFile('Home.html',function (error,data) {
+        fs.readFile('Home.html',function (error,data) {         //filepath,FileOption and callback Function
             res.writeHead(200,{'Content-type':'text/html'});
             res.write(data);
             res.end();

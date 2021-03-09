d3bf254d0d32bfb8956bc70381bbac77ea9792df
taimul <taimul.haque@gmail.com>
2021-03-09 20:28:23 +0600

    index file modified

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,7 +4,11 @@ var http=require('http');
 var server=http.createServer(function (req,res) {
 
     if(req.url="/"){
+<<<<<<< HEAD
             let mydata = fs.reaFileSync('Home.html');          //Synchronous, (no callback)
+=======
+        fs.readFile('Home.html',function (error,data) {         //filepath,FileOption and callback Function
+>>>>>>> 49b7993c8e2dbf4fd0bcf0245a2250a9b4a5895e
             res.writeHead(200,{'Content-type':'text/html'});
             res.write(mydata);
             res.end();

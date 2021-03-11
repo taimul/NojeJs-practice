664d0a1d7ffb7ea70f2e09b13b02a9e166b7c560
taimul <taimul.haque@gmail.com>
2021-03-11 13:23:45 +0600

    Node with mysql created

---
+++ Node_with_MySQL/index.js
@@ -0,0 +1,18 @@
+var mysql=require('mysql');
+
+var DatabaseConnectionConfig={
+    host:"localhost",
+    user:"root",
+    password:""
+}
+var con=mysql.createConnection(DatabaseConnectionConfig);
+
+
+con.connect(function (error) {
+    if (error){
+        console.log("connection failed")
+    }
+    else{
+        console.log("Connection Successful")
+    }
+});
--- fsModule/index.js
+++ fsModule/index.js
@@ -3,24 +3,12 @@ var http=require('http');
 
 var server=http.createServer(function (req,res) {
 
-    if(req.url="/"){
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            let mydata = fs.reaFileSync('Home.html');          //Synchronous, (no callback)
-=======
-        fs.readFile('Home.html',function (error,data) {         //filepath,FileOption and callback Function
->>>>>>> 49b7993c8e2dbf4fd0bcf0245a2250a9b4a5895e
-=======
-        fs.readFile('Home.html',function (error,data) {         //filepath,FileOption and callback Function
->>>>>>> 49b7993c8e2dbf4fd0bcf0245a2250a9b4a5895e
-=======
-        fs.readFile('Home.html',function (error,data) {         //filepath,FileOption and callback Function
->>>>>>> 49b7993c8e2dbf4fd0bcf0245a2250a9b4a5895e
-            res.writeHead(200,{'Content-type':'text/html'});
-            res.write(mydata);
+    if(req.ulr="/"){
+            fs.readFile('Home.html',function (error,data) {         //filepath,FileOption and callback Function
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.write(data);
             res.end();
-
+        });
     }
 });
 server.listen(4040);
